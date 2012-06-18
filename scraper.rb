97b5b9f967ec4e5db854d9b3f6d592ba4f45b33d
J. Pablo Pérez Trabucco <jpperez@ciudadanointeligente.org>
2012-06-18 15:56:14 -0400

    Updates scraper

--- scraper.rb
+++ scraper.rb
@@ -9,6 +9,8 @@ class ForestalTableScrapper
 		@source_sheet_osinfor_eia = 'docs/db_sheet2.csv'
 		@source_sheet_osinfor_conclusions = 'docs/db_sheet3.csv'
 		@data = []
+		@current_exportation_id = nil
+		@current_batches_ids = Array.new
 		@exportations_url = 'http://api.ciudadanointeligente.cl/forestal/pe/exportations'
 		@batches_url = 'http://api.ciudadanointeligente.cl/forestal/pe/batches'
 		@supervision_report_url = 'http://api.ciudadanointeligente.cl/forestal/pe/supervision_reports'
@@ -18,22 +20,30 @@ class ForestalTableScrapper
 			@data << row
 		end
 		for i in 1..@data.length
-			if !@data[i][2].nil?
-				#If you haven't num_exportation it's treated as a batch
+			if !@data[i][2].nil? #enter only if is a new exportation
+				if !@current_batches_ids.empty? #enter to insert to the exportation collection
+					RestClient.post @exportations_url, {:id => @current_exportation_id, :batches_ids => @current_batches_ids}, {:content_type => :json}
+					@current_exportation_id = []
+					@current_exportation_id = nil
+				end
+				if @current_exportation_id.nil?
+					@current_exportation_id = i
+				end
 				exportation_data = {
-				:num_exportation => @data[i][2],
-				:year => @data[i][1],
-				:num_cites => @data[i][3],
-				:date => @data[i][4],
-				:exporting_company => @data[i][5],
-				:importing_company => @data[i][6],
-				:destination_country => @data[i][7],
-				:species => @data[i][8],
-				:product_type => @data[i][9],
-				:shipping_volume => @data[i][10],
-				#:batches_ids => @data[i][],
-			}
+					:id => i,
+					:num_exportation => @data[i][2],
+					:year => @data[i][1],
+					:num_cites => @data[i][3],
+					:date => @data[i][4],
+					:exporting_company => @data[i][5],
+					:importing_company => @data[i][6],
+					:destination_country => @data[i][7],
+					:species => @data[i][8],
+					:product_type => @data[i][9],
+					:shipping_volume => @data[i][10],
+				}
 			end
+			@current_batches_ids << i
 			batch_data = {
 				:id => i,
 				:forestal_transport_guide => @data[i][11],
@@ -47,7 +57,7 @@ class ForestalTableScrapper
 				:observation => @data[i][20],
 			}
 
-			RestClient.put @exportations_url, exportation_data, {:content_type => :json}
+			#RestClient.put @exportations_url, exportation_data, {:content_type => :json}
 			RestClient.put @batches_url, batch_data, {:content_type => :json}
 		end
 	end
@@ -64,7 +74,7 @@ class ForestalTableScrapper
 				:contract_code => @data[i][2],
 				:concession => @data[i][3],
 				:department => @data[i][4],
-				:conclusion => @data[i][5], #.gsub(/\n/,''),
+				:conclusion => @data[i][5], #.gsub(/\n/,''), #use the gsub if you want remove the newline character
 			}
 
 			RestClient.put @supervision_report_url, supervision_report_data, {:content_type => :json}
