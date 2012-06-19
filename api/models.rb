5fa52a37952458125f4467f2e7c7499675f3adf4
J. Pablo Pérez Trabucco <jpperez@ciudadanointeligente.org>
2012-06-19 17:08:18 -0400

    Updates models to work with billit-generic-web

--- api/models.rb
+++ api/models.rb
@@ -1,62 +1,71 @@
+# coding: utf-8
 class Exportation
   include Mongoid::Document
 
-  # Relations
-  has_many :batches
+  # Field Validation
+  validates_presence_of :id
+  validates_uniqueness_of :id
 
   # Fields
-  field :id, :class => String
-  field :num_exportation, :class => String
-  field :year, :class => Date
-  field :num_cites, :class => Integer
-  field :date, :class => Date
-  field :exporting_company, :class => String
-  field :importing_company, :class => String
-  field :destination_country, :class => String
-  field :species, :class => String
-  field :product_type, :class => String
-  field :shipping_volume, :class => Float
-  field :batches_ids, :class => Array
+  field :id, :class => String ,:meta => ['display_name'=>'Id', 'type'=>'false', 'order'=>0, 'should_be_shown_in_list'=>false]
+  field :num_exportation, :class => String ,:meta => ['display_name'=>'Nº', 'type'=>'text', 'order'=>1, 'should_be_shown_in_list'=>true]
+  field :year, :class => Date ,:meta => ['display_name'=>'Año', 'type'=>'date', 'order'=>2, 'should_be_shown_in_list'=>true]
+  field :num_cites, :class => Integer ,:meta => ['display_name'=>'Número de cites', 'type'=>'text', 'order'=>3, 'should_be_shown_in_list'=>true]
+  field :date, :class => Date ,:meta => ['display_name'=>'Fecha', 'type'=>'date', 'order'=>4, 'should_be_shown_in_list'=>true]
+  field :exporting_company, :class => String ,:meta => ['display_name'=>'Empresa exportadora', 'type'=>'text', 'order'=>5, 'should_be_shown_in_list'=>true]
+  field :importing_company, :class => String ,:meta => ['display_name'=>'Empresa importadora', 'type'=>'text', 'order'=>6, 'should_be_shown_in_list'=>true]
+  field :destination_country, :class => String ,:meta => ['display_name'=>'País de destino', 'type'=>'text', 'order'=>7, 'should_be_shown_in_list'=>true]
+  field :species, :class => String ,:meta => ['display_name'=>'Especie', 'type'=>'text', 'order'=>8, 'should_be_shown_in_list'=>true]
+  field :product_type, :class => String ,:meta => ['display_name'=>'Tipo de producto', 'type'=>'text', 'order'=>9, 'should_be_shown_in_list'=>false]
+  field :shipping_volume, :class => Float ,:meta => ['display_name'=>'Volumen total del cargamento (m3)', 'type'=>'text', 'order'=>10, 'should_be_shown_in_list'=>false]
+  field :batches_ids, :class => Array ,:meta => ['display_name'=>'Id de lotes', 'type'=>'text', 'order'=>11, 'should_be_shown_in_list'=>false]
 
   # Indexes
   index :id, :unique => true
   index :num_exportation
+
+  # Relations
+  has_many :batches
 end
 
 class Batch
   include Mongoid::Document
 
-  # Relations
-  belongs_to :exportation
-
-  # Fields
-  field :id, :class => String
-  field :forestal_transport_guide, :class => String
-  field :contract_code, :class => String
-  field :contract_holder, :class => String
-  field :volume, :class => Float
-  field :zafra, :class => String
-  field :proprietary, :class => String
-  field :receiver, :class => String
-  field :basin, :class => String
-  field :observation, :class => String
+  #Fields
+  field :id, :class => String ,:meta => ['display_name'=>'Id', 'type'=>'text', 'order'=>0, 'should_be_shown_in_list'=>false]
+  field :forestal_transport_guide, :class => String ,:meta => ['display_name'=>'GTF', 'type'=>'text', 'order'=>1, 'should_be_shown_in_list'=>true]
+  field :contract_code, :class => String ,:meta => ['display_name'=>'Contrato', 'type'=>'text', 'order'=>2, 'should_be_shown_in_list'=>true]
+  field :contract_holder, :class => String ,:meta => ['display_name'=>'Titular', 'type'=>'text', 'order'=>3, 'should_be_shown_in_list'=>true]
+  field :volume, :class => Float ,:meta => ['display_name'=>'Volumen', 'type'=>'text', 'order'=>4, 'should_be_shown_in_list'=>true]
+  field :zafra, :class => String ,:meta => ['display_name'=>'Zafra', 'type'=>'text', 'order'=>5, 'should_be_shown_in_list'=>true]
+  field :proprietary, :class => String ,:meta => ['display_name'=>'Propietario', 'type'=>'text', 'order'=>6, 'should_be_shown_in_list'=>true]
+  field :receiver, :class => String ,:meta => ['display_name'=>'Destinatario', 'type'=>'text', 'order'=>7, 'should_be_shown_in_list'=>true]
+  field :basin, :class => String ,:meta => ['display_name'=>'Cuenca', 'type'=>'text', 'order'=>8, 'should_be_shown_in_list'=>true]
+  field :observation, :class => String ,:meta => ['display_name'=>'Observación', 'type'=>'text', 'order'=>9, 'should_be_shown_in_list'=>true]
 
   # Indexes
   index :id, :unique => true
+
+  # Relations
+  belongs_to :exportation
 end
 
 class SupervisionReport
   include Mongoid::Document
 
+  # Field Validation
+  validates_presence_of :id
+  validates_uniqueness_of :id
+
   # Fields
-  field :id, :class => String
-  field :supervision_report_code, :class => String
-  field :contract_holder, :class => String
-  field :contract_code, :class => String
-  field :concession, :class => String
-  field :department, :class => String
-  field :conclusion, :class => String
-  field :priority, :class => String
+  field :id, :class => String ,:meta => ['display_name'=>'Id', 'type'=>'text', 'order'=>0, 'should_be_shown_in_list'=>false]
+  field :supervision_report_code, :class => String ,:meta => ['display_name'=>'Nº de informe', 'type'=>'text', 'order'=>1, 'should_be_shown_in_list'=>true]
+  field :contract_holder, :class => String ,:meta => ['display_name'=>'Titular del contrato', 'type'=>'text', 'order'=>2, 'should_be_shown_in_list'=>true]
+  field :contract_code, :class => String ,:meta => ['display_name'=>'Nº de contrato', 'type'=>'text', 'order'=>3, 'should_be_shown_in_list'=>true]
+  field :concession, :class => String ,:meta => ['display_name'=>'Tipo de concesión', 'type'=>'text', 'order'=>4, 'should_be_shown_in_list'=>true]
+  field :department, :class => String ,:meta => ['display_name'=>'Departamento', 'type'=>'text', 'order'=>5, 'should_be_shown_in_list'=>true]
+  field :conclusion, :class => String ,:meta => ['display_name'=>'Conclusión', 'type'=>'text', 'order'=>6, 'should_be_shown_in_list'=>true]
+  field :priority, :class => String ,:meta => ['display_name'=>'Prioridad', 'type'=>'text', 'order'=>7, 'should_be_shown_in_list'=>true]
 
   # Indexes
   index :id, :unique => true
